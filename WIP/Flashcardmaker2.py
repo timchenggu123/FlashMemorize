bec1c6031c54f1bfc9683850081a8535e7e79fa7
18385963 <2013tim.g@gmail.com>
2018-10-20 10:37:01 -0400

    added answer evaluation function

--- WIP/Flashcardmaker2.py
+++ WIP/Flashcardmaker2.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import random as rd
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QMainWindow,QFrame
 from PyQt5.QtCore import pyqtSlot
 from tkinter.filedialog import askopenfilename
 
@@ -41,6 +41,12 @@ class card:
             return self.front
         else:
             return self.back
+    def getStats(self):
+        if self.timesStudied != 0:
+            return self.timesCorrect/self.timesStudied
+        else:
+            return 0
+    
     
 class deck:
 # The deck class' main purpose is to hold and manage a list of card objects
@@ -49,6 +55,7 @@ class deck:
     # name<string/int>: a unique name for the deck
     # cards<list>: a list containing card objects
         self.cards = cards
+        self.subDeck = list()
         self.size = len(cards)
         self.order = list(range(len(cards))) # A list containing the order by which the cards are sorted
         self.name = name
@@ -88,7 +95,22 @@ class deck:
             dk.append(self.cards[i])
         
         return dk
-
+    
+    def newSubdeck(self,subIDs):
+        self.subDeck.append(deck(self.cards[indx] for indx in subIDs))
+    
+    def deckStats(self):
+        #returns the overall correct rate of the deck
+        a = 0
+        b = 0
+        for i in self.cards:
+            a = a + i.timesCorrect
+            b = b + i.timesStudied
+        if b != 0:
+            return [a,b]
+        else:
+            return 0
+        
 class mainProgram(QWidget):
     
     def __init__(self,deck):
@@ -97,28 +119,34 @@ class mainProgram(QWidget):
         
         self.dk = deck
         self.i = 0           # a counter keeping track of where we are in the deck
+        self.correct = 1
         
         self.initUI()
         
         
     def initUI(self):
         # The main canvas
-        self.label = QLabel('',self)
+        self.stats = QLabel('                                               ',self)
+        self.stats.setGeometry(1,1,400,20)
+        self.canvas = QLabel('',self)
+        self.canvas.setGeometry(30,30,400,200)
+        self.canvas.setFrameStyle(QFrame.Panel)
         
         # Shuffle cards
         self.shuffle()
-        
+        self.updateStats()
         # The buttons 
-        btnNext = QPushButton('Next')
+        btnNext = QPushButton('Good')
         btnFlip = QPushButton('Flip')
         btnPrev = QPushButton('Previous',self)
         btnShuf = QPushButton('Shuffle',self)
+        btnBad = QPushButton('Bad',self)
         
         btnPrev.clicked.connect(self.Prev)
         btnFlip.clicked.connect(self.Flip)
-        btnNext.clicked.connect(self.Next)
+        btnNext.clicked.connect(self.Good)
         btnShuf.clicked.connect(self.shuffle)
-        
+        btnBad.clicked.connect(self.Bad)
                 
         # Create Boxes
         hbox = QHBoxLayout()
@@ -126,6 +154,7 @@ class mainProgram(QWidget):
         hbox.addWidget(btnPrev)
         hbox.addWidget(btnFlip)
         hbox.addWidget(btnNext)
+        hbox.addWidget(btnBad)
         hbox.addWidget(btnShuf)
         
         vbox = QVBoxLayout()
@@ -134,10 +163,10 @@ class mainProgram(QWidget):
         
         self.setLayout(vbox)
         
-        self.setGeometry(300,300,300,220)
-        self.setWindowTitle('SmartFlashCard')
-        
-        self.show()
+#        self.setGeometry(300,300,300,220)
+#        self.setWindowTitle('SmartFlashCard')
+#        
+#        self.show()
     
     def readCard(self,i):
     #Return the text on the current side of card[i]
@@ -146,26 +175,33 @@ class mainProgram(QWidget):
     
     def showCard(self):
     #Display the text on canvas
-        self.label.setText(self.readCard(self.i)) #
+        self.canvas.setText(self.readCard(self.i)) #
         
-    @pyqtSlot()
-    def Next(self):
-    # pyqt slot. show next card in deck on the canvas
+    def moveCard(self,step):
+        
+        self.updateStats()
         ncards = self.dk.size
-        self.i = self.i + 1
+        self.i = self.i + step
         if self.i == ncards:
             self.i = 0
+        elif self.i < 0:
+            self.i = self.dk.size - 1
             
+        
+    
+    @pyqtSlot()
+    def Good(self):
+    # pyqt slot. show next card in deck on the canvas
+    
+        self.moveCard(1)
         self.showCard()
         
     @pyqtSlot()
     def Prev(self):
     # pyqt slot. show previous card on canvas
     # fuck you xinghao
-        self.i = self.i - 1
-        if self.i < 0:
-            self.i = self.dk.size - 1
-            
+    
+        self.moveCard(-1)
         self.showCard()
     
     @pyqtSlot()
@@ -182,7 +218,49 @@ class mainProgram(QWidget):
         self.cards = self.dk.getdeck()
         self.showCard()
     
+    @pyqtSlot()
+    def Bad(self):
+    # update the current card status to be wrong
+        self.correct = 0
+        self.moveCard(1)
+        self.showCard()
+        print('marked')
     
+    def updateStats(self):
+    #update the times correct stats of a card
+    # correct <int> [0,1]. 0 represent the answer is wrong, 1 represent the answer is correct
+        currentId= self.cards[self.i].id
+        self.dk.cards[currentId].timesStudied = self.dk.cards[currentId].timesStudied + 1
+        self.dk.cards[currentId].timesCorrect = self.dk.cards[currentId].timesCorrect + self.correct
+        deckStats = self.dk.deckStats()
+        self.stats.setText('Cards Correct: ' + str(deckStats[0]) + ' Cards Studied:  ' 
+                           +  str(deckStats[1]) + ' Total Cards: ' + str(self.dk.size))
+        self.correct = 1
+        
+# test
+class mainWindow(QMainWindow):
+    
+    def __init__(self,deck):
+        super().__init__()
+        self.deck = deck
+        self.initUI()
+        
+        
+    def initUI(self):               
+        
+        self.mp = mainProgram(self.deck)
+        self.setCentralWidget(self.mp)
+        
+        self.statusBar().showMessage('Ready')
+        
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Statusbar')    
+        self.show()
+        
+    def showStats(self):
+        stats = self.mp.dk.deckStats()
+        self.statusBar().showMessage(str(stats[0]) + '/' + str(stats[1]))
+        
 #Prompt user to select source file & extract lines from source file
 tk.Tk().withdraw()  
 filename = askopenfilename()
@@ -210,5 +288,5 @@ cards = dk.getdeck()
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    ex = mainProgram(dk)
+    ex = mainWindow(dk)
 sys.exit(app.exec_())
